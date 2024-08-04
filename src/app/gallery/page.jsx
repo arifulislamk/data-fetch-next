32b775c30a61add307816a47f79e671cce23d7c8
arifulislamk <ariful18374@gmail.com>
2024-08-04 09:02:42 +0600

    error solv

--- src/app/gallery/page.jsx
+++ src/app/gallery/page.jsx
@@ -6,7 +6,7 @@ const page = () => {
           <h2 className=" text-center text-3xl font-semibold">All Image</h2>  
           <div>
             {
-                [1,2,3,4,5].map((image) => ( <Image key={image} src={`/image/${image}.jpg`} width={1080} height={1920}/>))
+                [1,2,3,4,5].map((image,inx) => ( <Image key={image}  alt={`Food image ${inx}`} src={`/image/${image}.jpg`} width={1080} height={1920}/>))
             }
           </div>
         </div>

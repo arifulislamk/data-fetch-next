2eaf1189918ef9f5cc8b6e1f7e3528fab402e1f6
arifulislamk <ariful18374@gmail.com>
2024-08-08 16:07:28 +0600

    add post request

--- src/app/api/user/route.js
+++ src/app/api/user/route.js
@@ -1,5 +1,34 @@
 export async function GET (){
     return Response.json({
-        message: "Welcome to the Next.js API!"
+        comment
     })
 }
+
+export async function POST (request){
+    const newcomment = await request.json();
+    comment.push({
+        id: comment.length + 1,
+        body: newcomment.body
+    });
+    return Response.json({
+        message: "Comment added successfully",
+        comment: comment
+    });
+}
+const comment = [ 
+    {
+        id: 1,
+        postId: 1,
+        body: "This is a sample comment"
+    },
+    {
+        id: 2,
+        postId: 2,
+        body: "This is a sample comment"
+    },
+    {
+        id: 1,
+        postId: 3,
+        body: "This is a sample comment"
+    },
+]
